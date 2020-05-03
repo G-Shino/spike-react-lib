aed500807359ff8e00748389fd5e818b43ab8619
G-shino <shinogu2929@gmail.com>
2020-05-03 17:04:16 +0900

    add new try

--- src/Spike-Exhibition/Try2/index.tsx
+++ src/Spike-Exhibition/Try2/index.tsx
@@ -1,34 +1,128 @@
 import * as React from "react";
-import { useState } from "react";
 import styles from "./style";
-import Yam from "./../../../data/Yam.jpg";
+import { useSprings } from "react-spring";
 
-const { WrapperDiv, ImageDiv, Image, Button } = styles;
+const {
+  WrapperDiv,
+  AnimatedBoxListsDiv,
+  AnimatedBoxDiv,
+  StyledButton,
+} = styles;
 
 interface FileInputProps {}
 
+// 上限と下限内にする
+const clamp = (x: number, min: number, max: number) => {
+  if (x < min) return min;
+  else if (x > max) return max;
+  else return x;
+};
+
+// 実際の要素数
+const ELEM_NUM = 7;
+// 表示するインデックス先頭と最後
+const DISP_IDX_START = 1;
+const DISP_IDX_END = 4;
+
+//対象要素の高さ
+const BOX_HEIGHT = 100;
+
 export const SpikeExhibitionTry2: React.FC<FileInputProps> = ({}) => {
-  const [flagClick, setFlagClick] = useState(false);
-  const [flagMouseOver, setFlagMouseOver] = useState(false);
+  //springsの作成と初期スタイルの設定
+  const [springs, setSprings] = useSprings(ELEM_NUM, (idx) => {
+    const dispPos =
+      (clamp(idx, DISP_IDX_START, DISP_IDX_END) - DISP_IDX_START) * BOX_HEIGHT;
+    const flagDisp = idx < DISP_IDX_START || DISP_IDX_END < idx;
+    return {
+      x: 0,
+      y: dispPos,
+      opacity: flagDisp ? 0 : 1,
+      zIndex: flagDisp ? 0 : 1,
+      scale: 1,
+    };
+  });
+  //配列を格納するため
+  const order = React.useRef(springs.map((_, index) => index));
+
+  //クリックした時用のsetSpringsに渡す、objを作る関数
+  const makeStyle = (idx: number) => {
+    const curIdx = order.current.indexOf(idx);
+    const y =
+      (clamp(curIdx, DISP_IDX_START, DISP_IDX_END) - DISP_IDX_START) *
+      BOX_HEIGHT;
+    const flagDisp = curIdx < DISP_IDX_START || DISP_IDX_END < curIdx;
+    const opacity = flagDisp ? 0 : 1;
+    const zIndex = flagDisp
+      ? 0
+      : curIdx === DISP_IDX_END || curIdx === DISP_IDX_START
+      ? 1
+      : 2;
+    const props = {
+      x: 0,
+      y,
+      opacity,
+      zIndex,
+      immediate: (n: string) => n === "zIndex",
+    };
+    return props;
+  };
+
+  //配列の先頭を最後尾にする
+  const handleUpperClick = () => {
+    const firstIdx = order.current.shift() as number;
+    order.current.push(firstIdx);
+    setSprings(makeStyle);
+  };
+
+  //配列の最後尾を先頭にする
+  const handleLowerClick = () => {
+    const lastIdx = order.current.pop() as number;
+    order.current.unshift(lastIdx);
+    setSprings(makeStyle);
+  };
+
+  //カーソル がのったときは要素を拡大する
+  const handleOnMouseEnter = (idx: number) => {
+    setSprings((i: number) => ({
+      scale: idx === i ? 1.2 : 1.0,
+    }));
+  };
+  const handleOnMouseLeave = () => {
+    setSprings((i: number) => ({
+      scale: 1.0,
+    }));
+  };
+
+  //クリックしたときに移動and拡大
+  const handleBoxClick = (idx: number) => {
+    setSprings((i: number) => ({
+      x: idx === i ? 500 : 0,
+    }));
+  };
+
   return (
     <WrapperDiv>
-      <p>Sample</p>
-      <ImageDiv
-        flagClick={flagClick}
-        flagMouseOver={flagMouseOver}
-        onClick={() => {
-          setFlagClick(true);
-        }}
-        onMouseEnter={() => {
-          setFlagMouseOver(true);
-        }}
-        onMouseLeave={() => {
-          setFlagMouseOver(false);
+      <StyledButton onClick={handleUpperClick}></StyledButton>
+      <AnimatedBoxListsDiv
+        style={{
+          height: `${BOX_HEIGHT * (DISP_IDX_END - DISP_IDX_START + 1)}px`,
         }}
       >
-        <Image src={Yam} />
-      </ImageDiv>
-      <Button onClick={() => setFlagClick(false)}>拡大解除</Button>
+        {springs.map((spring, idx) => (
+          <AnimatedBoxDiv
+            key={idx}
+            style={{ ...spring }}
+            onMouseEnter={() => {
+              handleOnMouseEnter(idx);
+            }}
+            onMouseLeave={handleOnMouseLeave}
+            onClick={() => {
+              handleBoxClick(idx);
+            }}
+          ></AnimatedBoxDiv>
+        ))}
+      </AnimatedBoxListsDiv>
+      <StyledButton onClick={handleLowerClick}></StyledButton>
     </WrapperDiv>
   );
 };
--- src/Spike-Exhibition/Try2/style.tsx
+++ src/Spike-Exhibition/Try2/style.tsx
@@ -1,43 +1,57 @@
 import styled from "styled-components";
+import { animated } from "react-spring";
 
 const WrapperDiv = styled.div`
-  height: 1000px;
-`;
-
-interface ImageDivProps {
-  flagClick: boolean;
-  flagMouseOver: boolean;
-}
-
-const ImageDiv = styled.div`
+  margin: 0 auto;
   width: 400px;
-  height: ${(props: ImageDivProps) => (props.flagClick ? 400 : 50)}px;
-  border: 1px solid;
-  transition: all 1s ease;
-  transform: translateX(
-      ${(props: ImageDivProps) => (props.flagClick ? 500 : 0)}px
-    )
-    scaleX(
-      ${(props: ImageDivProps) =>
-        props.flagMouseOver && !props.flagClick ? 1.2 : 1}
-    )
-    scaleY(
-      ${(props: ImageDivProps) =>
-        props.flagMouseOver && !props.flagClick ? 1.2 : 1}
-    );
-`;
-
-const Button = styled.button``;
-
-const Image = styled.img`
-  width: 100%;
   height: 100%;
-  object-fit: cover;
+`;
+const BaseBoxListsDiv = styled.div`
+  position: relative;
+  width: 100%;
+  & > div:nth-child(1) {
+    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
+  }
+  & > div:nth-child(2) {
+    background: linear-gradient(135deg, #f093fb 0%, #f5576c 100%);
+  }
+  & > div:nth-child(3) {
+    background: linear-gradient(135deg, #5ee7df 0%, #b490ca 100%);
+  }
+  & > div:nth-child(4) {
+    background: linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%);
+  }
+  & > div:nth-child(5) {
+    background: linear-gradient(135deg, #bcfa89 0%, #049336 100%);
+  }
+  & > div:nth-child(6) {
+    background: linear-gradient(135deg, #dc143c 0%, #ff69b4 100%);
+  }
+  & > div:nth-child(7) {
+    background: linear-gradient(135deg, #0000aa 0%, #bdbeec 100%);
+  }
+`;
+const AnimatedBoxListsDiv = animated(BaseBoxListsDiv);
+
+const BaseBoxDiv = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 80px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+`;
+const AnimatedBoxDiv = animated(BaseBoxDiv);
+
+const StyledButton = styled.button`
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
 `;
 
 export default {
   WrapperDiv,
-  ImageDiv,
-  Image,
-  Button,
+  AnimatedBoxListsDiv,
+  AnimatedBoxDiv,
+  StyledButton,
 };
